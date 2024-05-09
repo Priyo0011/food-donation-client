4037e75ad52f3d113a97532991b55082c3735f4e
Priyo0011 <priyotosh0011@gmail.com>
2024-05-10 00:16:46 +0600

    added banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,56 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import Slide from './Slide'
+
+import bgimg1 from '../assets/images/d.img3.jpg'
+import bgimg2 from '../assets/images/d.img1.jpg'
+import bgimg3 from '../assets/images/d.img2.jpg'
+
+export default function Banner() {
+  return (
+    <div className='py-10'>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className='mySwiper'
+      >
+        <SwiperSlide>
+          <Slide
+            image={bgimg1}
+            text='Build a Beautiful World.  Charity organizations.'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide
+            image={bgimg2}
+            text='We Are Be aHand.  We Are a Helping Hand.'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide
+            image={bgimg3}
+            text='Helping Hand.  We Build a Fundraising For Help Less People.'
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
---
+++ src/components/Slide.jsx
@@ -0,0 +1,25 @@
+const Slide = ({ image, text }) => {
+  return (
+    <div
+      className=" bg-center bg-cover md:h-[660px] h-52"
+      style={{
+        backgroundImage: `url(${image})`,
+      }}
+    >
+      <div className="flex items-center justify-center w-full h-full bg-gray-900/70">
+        <div className="text-center w-40 md:w-full">
+          <h1 className="md:text-3xl font-semibold text-white">{text}</h1>
+          <br />
+          <button className="btn btn-ghost border-white hover:bg-[#db4437] text-white font-bold mr-6">
+            Learn More
+          </button>
+          <button className="btn btn-ghost border-white hover:bg-[#db4437] text-white font-bold">
+            About Us
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
